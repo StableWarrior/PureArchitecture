d16c717e7f63093f23d628edae20979588f17d8a
Глеб Кибяков <gleserki@gmail.com>
2026-03-22 21:10:48 +1000

    Реализовал Outbox, Inbox repository

---
+++ src/database/repository/inbox_repository.py
@@ -0,0 +1,36 @@
+from typing import Any
+from uuid import UUID
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
+
+from ..models import Inbox
+
+
+class InboxRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def save(
+        self, event_type: str, payload: dict[str, Any], status: str
+    ):
+
+        inbox = Inbox(
+            event_type=event_type,
+            payload=payload,
+            status=status,
+        )
+        self.db.add(inbox)
+
+        return inbox
+
+    async def get(self, status: str):
+        result = await self.db.execute(
+            select(Inbox)
+            .where(Inbox.status == status)
+        )
+
+        inboxes = result.scalars().all()
+
+        return inboxes
