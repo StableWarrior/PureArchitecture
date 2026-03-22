1705ce81dab7951ebe2dbf08d1fdbfe65d677bdf
Глеб Кибяков <gleserki@gmail.com>
2026-03-22 21:12:42 +1000

    Обновил зависимости, env

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+python -m src.core.scheduler_outbox &
+python -m src.core.scheduler_inbox &
 uvicorn src.main:app --host 0.0.0.0 --port 8000 &
 
 wait
--- src/infastructure/session.py
+++ src/infastructure/session.py
@@ -1,12 +1,16 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..database.repository.order_repository import OrderRepository
+from ..database.repository.outbox_repository import OutboxRepository
+from ..database.repository.inbox_repository import InboxRepository
 
 
 class Session:
     def __init__(self, session: AsyncSession):
         self.session = session
         self.order_repo = OrderRepository(session)
+        self.outbox_repo = OutboxRepository(session)
+        self.inbox_repo = InboxRepository(session)
 
     async def __aenter__(self):
         await self.session.begin()
@@ -34,3 +38,13 @@ class Session:
     def orders(self) -> OrderRepository:
         """Доступ к OrderRepository"""
         return self.order_repo
+
+    @property
+    def outbox(self) -> OutboxRepository:
+        """Доступ к OutboxRepository"""
+        return self.outbox_repo
+
+    @property
+    def inbox(self) -> InboxRepository:
+        """Доступ к InboxRepository"""
+        return self.inbox_repo
